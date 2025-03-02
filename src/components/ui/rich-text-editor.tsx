7cc23dcf2105bd64a5f7ba22d6bb932afae4c33a
Arnau Gómez <agomez1f@gmail.com>
2025-03-02 11:07:04 +0100

    fix: placeholder color

--- src/components/ui/rich-text-editor.tsx
+++ src/components/ui/rich-text-editor.tsx
@@ -78,7 +78,7 @@ export function RichTextEditor({
     editorProps: {
       attributes: {
         class: cn(
-          "block border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "block border-input focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           "prose prose-sm sm:prose-base max-w-full"
         ),
       },
